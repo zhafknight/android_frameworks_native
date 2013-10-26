319a1e644bb811f1c4f7c05707e0a699e9976762
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-10-26 03:43:03 +0100

    Support forcing all screenshots into a CPU consumer
    Looks like a full GPU path is less efficient on some GPU
    drivers that we're still using, and CPU is reliably faster...
    (there's probably a locking condition going on somewhere, this
    needs to be looked into)
    
    Change-Id: I8878796a117d65bf2324507cf8755cadce49f6dc

--- libs/gui/SurfaceComposerClient.cpp
+++ libs/gui/SurfaceComposerClient.cpp
@@ -940,6 +940,12 @@ status_t SurfaceComposerClient::getHdrCapabilities(const sp<IBinder>& display,
 
 // ----------------------------------------------------------------------------
 
+#ifndef FORCE_SCREENSHOT_CPU_PATH
+#define SS_CPU_CONSUMER false
+#else
+#define SS_CPU_CONSUMER true
+#endif
+
 status_t ScreenshotClient::capture(
         const sp<IBinder>& display,
         const sp<IGraphicBufferProducer>& producer,
@@ -949,7 +955,7 @@ status_t ScreenshotClient::capture(
     if (s == NULL) return NO_INIT;
     return s->captureScreen(display, producer, sourceCrop,
             reqWidth, reqHeight, minLayerZ, maxLayerZ, useIdentityTransform,
-            ISurfaceComposer::eRotateNone, false);
+            ISurfaceComposer::eRotateNone, SS_CPU_CONSUMER);
 }
 
 status_t ScreenshotClient::captureToBuffer(const sp<IBinder>& display,
@@ -969,7 +975,7 @@ status_t ScreenshotClient::captureToBuffer(const sp<IBinder>& display,
 
     status_t ret = s->captureScreen(display, producer, sourceCrop, reqWidth, reqHeight,
             minLayerZ, maxLayerZ, useIdentityTransform,
-            static_cast<ISurfaceComposer::Rotation>(rotation));
+            static_cast<ISurfaceComposer::Rotation>(rotation), SS_CPU_CONSUMER);
     if (ret != NO_ERROR) {
         return ret;
     }
@@ -1014,7 +1020,7 @@ status_t ScreenshotClient::update(const sp<IBinder>& display,
 
     status_t err = s->captureScreen(display, mProducer, sourceCrop,
             reqWidth, reqHeight, minLayerZ, maxLayerZ, useIdentityTransform,
-            static_cast<ISurfaceComposer::Rotation>(rotation), true);
+            static_cast<ISurfaceComposer::Rotation>(rotation), SS_CPU_CONSUMER);
 
     if (err == NO_ERROR) {
         err = mCpuConsumer->lockNextBuffer(&mBuffer);
