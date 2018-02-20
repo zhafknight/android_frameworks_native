351a5677139ed27cfe554778b69decba986af76e
Dominggoes Isakh <drjisakh@gmail.com>
2018-02-20 21:56:49 +0100

    GraphicBufferMapper: Add getphys for Exynos4
    Change-Id: I9a66b2ed6f963781f941bf8d5afad767f3454b3a

--- libs/ui/include/ui/GraphicBufferMapper.h
+++ libs/ui/include/ui/GraphicBufferMapper.h
@@ -61,6 +61,8 @@ public:
 
     status_t unlock(buffer_handle_t handle);
 
+    status_t getphys(buffer_handle_t handle, void** paddr);
+
     status_t lockAsync(buffer_handle_t handle,
             uint32_t usage, const Rect& bounds, void** vaddr, int fenceFd);
 
@@ -71,6 +73,7 @@ public:
     status_t lockAsyncYCbCr(buffer_handle_t handle,
             uint32_t usage, const Rect& bounds, android_ycbcr *ycbcr,
             int fenceFd);
+
 
     status_t unlockAsync(buffer_handle_t handle, int *fenceFd);
 
